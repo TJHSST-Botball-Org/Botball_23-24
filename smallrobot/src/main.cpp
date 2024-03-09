562565ecdcfd4391c17096dc7067436f9ed6b02b
2025aperakal <2025aperakal@tjhsst.edu>
2024-03-09 15:56:58 -0500

    first commit

--- smallrobot/src/main.cpp
+++ smallrobot/src/main.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 #include "include/OpMode.hpp"
+#include <SmallRobot.hpp>
+
+int main() 
+{
+    //Easy Robot Route (All Items on board, Optional: Return to Starting box at the end )
+    SmallRobot robot(0, 1);
+
+    robot.
+    return 0;
+}
