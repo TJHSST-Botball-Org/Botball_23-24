91f1992981ac3737402940496e9d2464adb7df79
BaconErie <baconerie0001@gmail.com>
2024-04-06 13:00:41 -0400

    Add new methods & properies in header file

--- smallrobot/include/SmallRobot.h
+++ smallrobot/include/SmallRobot.h
@@ -14,21 +14,28 @@ private:
     int rightPosPerOneInch;
     int leftLightPin;
     int rightLightPin;
+    int clawServoPin;
+    int armServoPin;
 
 public:
-    SmallRobot(int leftWheelPin, int rightWheelPin, float wheelDistance, float wheelRadius, int leftTicksPerRevolution, int rightTicksPerRevolution, int leftColorPin = -1, int rightColorPin = -1);
+    SmallRobot(int leftWheelPin, int rightWheelPin, float wheelDistance, float wheelRadius, int leftTicksPerRevolution, int rightTicksPerRevolution, int leftColorPin = -1, int rightColorPin = -1, int clawServoPin, int armServoPin);
 
     void moveDistanceAndCorrect(float distance, int ticksPerSecond, bool condition = true); // Moves for some distance, blocking
     void moveDistance(float distance, int ticksPerSecond, bool condition = true);           // Same as moveForwardDistanceAndCorrect, but does not correct
 
-    void moveContinuous(int percentPower); // Moves continously forward/backwards. percentPower -100 to 100
-    void moveUntilBlackLine(int percentPower);//Blocking, and goes forward until both light sensors see black lines
+    void moveContinuous(int percentPower);     // Moves continously forward/backwards. percentPower -100 to 100
+    void moveUntilBlackLine(int percentPower); // Blocking, and goes forward until both light sensors see black lines
 
     void rotateAndCorrect(int degrees, int ticksPerSecond, bool condition = true); // Rotates a certain amount of degrees clockwise and corrects. Accepts values from -360 to 360. Blocking.
     void rotate(int degrees, int percentPower, bool condition = true);             // Rotates a certain amount of degrees clockwise. Accepts values from -360 to 360. Blocking.
 
     void turnLeftContinuous(int percentPower);  // Turns left continously. Percent power 0-100
     void turnRightContinuous(int percentPower); // Turns right continously. Percent power 0-100
+
+    void setClawPosition(int pos, int speed);
+    void setArmPosition(int pos, int speed);
+
+    void grabBotguy();
 
     void stop(); // Stops all turns and forward/backward movement.
 
